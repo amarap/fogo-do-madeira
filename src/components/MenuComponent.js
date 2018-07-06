5989467898e0247057c796b18abda5646c2ee453
amarap <pedro.amaral.urosa@gmail.com>
2018-07-05 21:42:47 -0400

    Components Part 1-1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
+import '../App.css';
 
 class Menu extends Component {
     constructor(props) {
@@ -8,32 +9,40 @@ class Menu extends Component {
             dishes: [
                 {
                   id: 0,
-                  name:'Uthappizza',
-                  image: 'assets/images/uthappizza.png',
+                  name:'Espetada',
+                  image: 'assets/espetada.png',
                   category: 'mains',
                   label:'Hot',
                   price:'4.99',
                   description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
                {
                   id: 1,
-                  name:'Zucchipakoda',
-                  image: 'assets/images/zucchipakoda.png',
+                  name:'Bacalao en Natas',
+                  image: 'assets/bacalao.png',
                   category: 'appetizer',
                   label:'',
                   price:'1.99',
                   description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
                {
                   id: 2,
-                  name:'Vadonut',
-                  image: 'assets/images/vadonut.png',
+                  name:'Chorizo Azado',
+                  image: 'assets/chorizo.png',
+                  category: 'appetizer',
+                  label:'New',
+                  price:'1.99',
+                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
+                               {
+                  id: 3,
+                  name:'Milho Frito',
+                  image: 'assets/milho.jpg',
                   category: 'appetizer',
                   label:'New',
                   price:'1.99',
                   description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
                {
-                  id: 3,
-                  name:'ElaiCheese Cake',
-                  image: 'assets/images/elaicheesecake.png',
+                  id: 4,
+                  name:'Pastel De Nata',
+                  image: 'assets/pastel-de-nata.png',
                   category: 'dessert',
                   label:'',
                   price:'2.99',
@@ -48,7 +57,7 @@ class Menu extends Component {
               <div key={dish.id} className="col-12 mt-5">
                 <Media tag="li">
                   <Media left middle>
-                      <Media object src={dish.image} alt={dish.name} />
+                      <Media object src={dish.image} alt={dish.name} className="imagen"/>
                   </Media>
                   <Media body className="ml-5">
                     <Media heading>{dish.name}</Media>
