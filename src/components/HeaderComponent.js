e9e466da980b4b2cf35a0c133eb97d53b9b0d998
amarap <pedro.amaral.urosa@gmail.com>
2018-07-08 20:02:52 -0400

    Footer and Header

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+
+class Header extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar dark>
+                    <div className="container">
+                        <NavbarBrand href="/">Fogo Do Madeira</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Jumbotron>
+                    <div className="container">
+                        <div className="row row-header">
+                            <div className="col-12 col-sm-6">
+                                <h1>Fogo Do Madeira</h1>
+                                <p>Authentic Madeira Cuisine that will make you crave for more until you explote!</p>
+                            </div>
+                        </div>
+                    </div>
+                </Jumbotron>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
